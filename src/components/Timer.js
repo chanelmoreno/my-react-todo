f2d66b57883879ad6772198912cf91edb7c79851
chanel donahoe <cdonahoe05@gmail.com>
2023-02-11 23:48:17 +1100

    updates

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import '../App.css';
 
 const Timer = () => {
+  let milliseconds = 1500000;
+  let deadline = Date.now() + milliseconds;
+  // on page load     1pm = 1.25pm 
+  // new time          1.05pm --> 1.30pm 
+
+  // if newtime + 25 minutes > on page load 
+  // update to new time 
+
+
+
   const [isTimerStarted, setisTimerStarted] = useState(false)
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
-  const milliseconds = 1500000 //25 minutes
-  let deadline = Date.now() + milliseconds
+
 
   const getTime = () => {
+    let newTime = Date.now() + milliseconds
 
+    if (newTime >= deadline) {
+      console.log(`newtime ${newTime}`)
+      // console.log(`deadline ${deadline}`)
+
+    }
     const time = (deadline) - Date.now();
+
     setMinutes(Math.floor((time / 1000 / 60) % 60));
     setSeconds(Math.floor((time / 1000) % 60));
+    // console.log(time)
   };
 
-  const pauseTimer = () => {
-    console.log(minutes)
-    console.log(seconds)
-  }
 
   const startTimer = () => {
     setisTimerStarted(true);
@@ -28,7 +41,6 @@ const Timer = () => {
 
   useEffect(() => {
     const interval = setInterval(() => getTime(), 1000);
-
     return () => clearInterval(interval);
   }, []);
 
@@ -47,10 +59,9 @@ const Timer = () => {
             </div>
           </div>
           <div className="timer" >
-            <button onClick={() => pauseTimer(minutes, seconds)} className='start-button'>🚫 Stop</button>
+            {/* <button onClick={() => pauseTimer(minutes, seconds)} className='start-button'>🚫 Stop</button> */}
           </div>
         </div>
-
       ) : (
         <div>
           <div className="timer">
@@ -73,3 +84,9 @@ const Timer = () => {
 };
 
 export default Timer;
+
+ // const pauseTimer = () => {
+  //   console.log(minutes)
+  //   console.log(seconds)
+  //   deadline = Date.now() + milliseconds;
+  // }
